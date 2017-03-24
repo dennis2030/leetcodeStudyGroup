be597d47d276d1f3ad639861880fe3420ca71ab2
wyrj <yrj0103@gmail.com>
2017-03-24 21:42:19 +0800

    wyrj - update 095

--- 95-unique-binary-search-trees-ii/wyrj.js
+++ 95-unique-binary-search-trees-ii/wyrj.js
@@ -5,16 +5,22 @@
  *     this.left = this.right = null;
  * }
  */
-function Node(left, right) {
-    this.left = left || null;
-    this.right = right || null;
-}
 /**
  * @param {number} n
  * @return {TreeNode[]}
  */
+TreeNode.prototype.assign = function(left, right) {
+    this.left = left;
+    this.right = right ? right.addon(this.val) : null;
+    return this;
+};
+TreeNode.prototype.addon = function(n) {
+    let node = new TreeNode(this.val + n);
+    if (this.left) node.left = this.left.addon(n);
+    if (this.right) node.right = this.right.addon(n);
+    return node;
+}
 var generateTrees = function(n) {
-    if (n === 0) return [];
     var trees = [], layer, i, leftIndex, leftLayer, rightIndex, rightLayer;
     trees[0] = [null];
     for (layer = 1; layer <= n; layer++) {
@@ -24,29 +30,10 @@ var generateTrees = function(n) {
             rightLayer = trees[layer - i - 1];
             for (leftIndex = 0; leftIndex < leftLayer.length; leftIndex++) {
                 for (rightIndex = 0; rightIndex < rightLayer.length; rightIndex++) {
-                    trees[layer].push(new Node(leftLayer[leftIndex], rightLayer[rightIndex]));
+                    trees[layer].push(new TreeNode(i + 1).assign(leftLayer[leftIndex], rightLayer[rightIndex]));
                 }
             }
         }
     }
-    var fillNumber = function(node, treeNode, number) {
-        if (node.left) {
-            treeNode.left = new TreeNode(0);
-            number = fillNumber(node.left, treeNode.left, number);
-        }
-        treeNode.val = number;
-        number += 1;
-        if (node.right) {
-            treeNode.right = new TreeNode(0);
-            number = fillNumber(node.right, treeNode.right, number);
-        }
-        return number;
-    };
-    var treeNodes = [];
-    trees[n].forEach(function(node) {
-        var treeNode = new TreeNode(0);
-        fillNumber(node, treeNode, 1);
-        treeNodes.push(treeNode);
-    });
-    return treeNodes;
+    return n === 0 ? [] : trees[n];
 };
