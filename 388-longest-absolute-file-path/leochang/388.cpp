c29b7bf4eea5a6068ae0b33d2a2c9c3df6e36db6
leochang <leochang@synology.com>
2017-10-02 00:06:29 +0800

    leo - 388 v2

--- 388-longest-absolute-file-path/leochang/388.cpp
+++ 388-longest-absolute-file-path/leochang/388.cpp
@@ -2,19 +2,20 @@ class Solution {
 public:
 	int lengthLongestPath(string input) {
 		int maxLen = 0;
-		map <int, int> len;
+		vector<int> len(100, 0);
 
 		for(int i = 0; i < input.size(); i++){
 			int count = 0, dirLevel = 1;
 			bool isFile = false;
-			while(input[i] == '\t' && i < input.size()){
-				dirLevel++;
-				i++;
-			}
 			while(input[i] != '\n' && i < input.size()){
 				if (input[i] == '.') isFile = true;
-				count++;
-				i++;
+				if (input[i] == '\t') {
+					dirLevel++;
+					i++;
+				} else {
+					count++;
+					i++;
+				}
 			}
 			if (isFile) {
 				maxLen = max(maxLen, len[dirLevel - 1] + count);
