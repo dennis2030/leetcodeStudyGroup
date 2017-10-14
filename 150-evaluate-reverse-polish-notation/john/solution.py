669e62315c0d75ad3b21b5c4cbeb8b56f7e840c5
John Lin <johnlinp@gmail.com>
2017-10-14 22:40:12 +0800

    john - 150

---
+++ 150-evaluate-reverse-polish-notation/john/solution.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        stack = []
+        for token in tokens:
+            if token not in ['+', '-', '*', '/']:
+                stack.append(int(token))
+            else:
+                num2 = stack.pop()
+                num1 = stack.pop()
+                if token == '+':
+                    stack.append(num1 + num2)
+                elif token == '-':
+                    stack.append(num1 - num2)
+                elif token == '*':
+                    stack.append(num1 * num2)
+                elif token == '/':
+                    sign = 1 if num1 * num2 >= 0 else -1
+                    stack.append(abs(num1) / abs(num2) * sign)
+                else:
+                    assert False
+
+        return stack[0]
