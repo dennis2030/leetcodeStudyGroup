eb9d3477c2f808048e1810ffb03b51f2afed1aee
Poplarysl <Poplarysl@users.noreply.github.com>
2017-12-24 01:44:40 +0800

    Sony - 621

--- 621-task-scheduler/Sony.py
+++ 621-task-scheduler/Sony.py
@@ -7,20 +7,16 @@ class Solution(object):
         """
         from collections import defaultdict
         task_num = len(tasks)
-        if task_num == 0:
-            return 0
         task_count = defaultdict(int)
         for task in tasks:
             task_count[task] += 1
-        task_count = task_count.items()
-        task_type_num = len(task_count)
-        task_count.sort(key=lambda x: -x[1])
-        
-        interval = 0
-        max_count = task_count[0][1]
+        max_count = 0
         max_count_task_num = 0
-        for task_count_item in task_count:
-            if task_count_item[1] < max_count:
-                break
-            max_count_task_num += 1
+        for task_count_item in task_count.items():
+            if task_count_item[1] > max_count:
+                max_count = task_count_item[1]
+                max_count_task_num = 1
+            elif task_count_item[1] == max_count:
+                max_count_task_num += 1
+
         return max((n + 1) * (max_count - 1) + max_count_task_num, task_num)
