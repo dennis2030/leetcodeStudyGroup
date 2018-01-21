445683fe2e332c420cbe9bd9de23bdcd8634adf6
fereshte <cheng198912@gmail.com>
2018-01-21 23:52:52 +0800

    leo - 240

---
+++ 240-search-a-2d-matrix-ii/leochang/240.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if (matrix.size() == 0 || matrix[0].size() == 0) return false;
+        int row = 0, col = matrix[0].size()-1;
+        while (row < matrix.size() && col >= 0) {
+            if (target == matrix[row][col]) {
+                return true;
+            } else if (target < matrix[row][col]) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+        return false;
+    }
+};
